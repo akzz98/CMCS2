88e1c09b05dc19ab113072579fec8add7ee0dad5
Alton Muganda <ST10263456@vcconnect.edu.za>
2024-10-10 16:23:03 +0200

    add Organization controller code

--- CMCS2/Controllers/OrganizationController.cs
+++ CMCS2/Controllers/OrganizationController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CMCS2.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CMCS2.Controllers
 {
     public class OrganizationController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public OrganizationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,17 +19,20 @@ namespace CMCS2.Controllers
 
         public ActionResult ViewUnapprovedClaims()
         {
-            return View();
+            var unapprovedClaims = _context.Claims.Where(c => c.Status == "Pending").ToList();
+            return View(unapprovedClaims);
         }
 
         public ActionResult ViewVerifiedClaims()
         {
-            return View();
+            var verifiedClaims = _context.Claims.Where(c => c.Status == "Verified").ToList();
+            return View(verifiedClaims);
         }
 
         public ActionResult ViewApprovedClaims()
         {
-            return View();
+            var approvedClaims = _context.Claims.Where(c => c.Status == "Approved").ToList();
+            return View(approvedClaims);
         }
     }
 }
